c78771fd6585432bc0f84ec68dbeac477889f1e8
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-04-14 22:30:06 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -11,4 +11,18 @@ class Owner
     @pets = {:fishes => [], :dogs => [], :cats => []}
   end
 
+  def self.all
+  @@all
+end
+
+def self.count
+  @@all.length
+end
+
+def self.reset_all
+  @@all.clear
+end
+
+  def
+
 end
