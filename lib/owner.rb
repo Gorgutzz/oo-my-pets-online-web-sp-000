32605a96c160d7b6ea7d2649f5352e39c8260fa5
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-04-14 22:45:27 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -58,11 +58,11 @@ class Owner
   end
 
   def sell_pets
-    @pets.each do |type, pets|
+    pets.each do |type, pets|
       pets.each do |pet|
         pet.mood = "nervous"
       end
-      @pets.clear
+    pets.clear
     end
   end
 
